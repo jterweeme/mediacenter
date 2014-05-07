68bfbbb7c46eb30b00535d091f4f58dd5f4797bb
Alex Aalbertsberg <alex.aalbertsberg@home.nl>
2014-05-07 11:32:41 +0200

    Audio zooi

--- software/zuul1/audiotest1.cpp
+++ software/zuul1/audiotest1.cpp
@@ -6,19 +6,16 @@ int main()
     alt_up_audio_dev *audio_dev;
     unsigned int l_buf, r_buf;
 
-    audio_dev = alt_up_audio_open_dev("/dev/Audio");
+	audio_dev = alt_up_audio_open_dev(AUDIO_0_NAME);
+    //audio_dev = alt_up_audio_open_dev("/dev/Audio");
 
     if (audio_dev == NULL)
         ::printf("Error: could not open audio device\n");
     else
         ::printf("Opened audio device\n");
 
-    unsigned int buf[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-
     while (1)
     {
-        alt_up_audio_play_r(audio_dev, buf, 19);
-/*
         int fifospace = ::alt_up_audio_read_fifo_avail(audio_dev, ALT_UP_AUDIO_RIGHT);
 
         if (fifospace > 0)
@@ -26,7 +23,6 @@ int main()
             ::alt_up_audio_read_fifo(audio_dev, &(r_buf), 1, ALT_UP_AUDIO_RIGHT);
             ::alt_up_audio_write_fifo(audio_dev, &(l_buf), 1, ALT_UP_AUDIO_RIGHT);
         }
-*/
     }
     return 0;
 }
