9a76086c941b2f9a78c8c494ba18d96603854ca6
Jasper ter Weeme <jasper@nbalternate.lan>
2014-05-17 18:38:03 +0200

    update diagram

--- software/zuul1/vgatest1.cpp
+++ software/zuul1/vgatest1.cpp
@@ -15,7 +15,23 @@ int VGATest::run()
     vga->clear();
     vga->puts("The Court consists of a chief justice and eight associate ");
     vga->puts("justices who are nominated by the President and confirmed by ");
-    vga->puts("the Senate. Once appointed, justices have life tenure unless\r\nthey resign, retire, take senior status, or are removed after impeachment (though none has ever been removed). In modern discourse, the justices are often categorized as having conservative, moderate, or liberal philosophies of law and of judicial interpretation. Each justice has one vote, and while many cases are decided unanimously, many of the highest profile cases often expose ideological beliefs that track with those philosophical or political categories. The Court meets in the United States Supreme Court Building in Washington, D.C.");
+    vga->puts("the Senate. Once appointed, justices have life ");
+    vga->puts("tenure unless\r\nthey resign, retire, take senior status, or ");
+    vga->puts("are removed after impeachment (though none has ever been ");
+    vga->puts("removed). In modern discourse, the justices are often categorized as having conservative, moderate, or liberal philosophies of law and of judicial interpretation. Each justice has one vote, and while many cases are decided unanimously, many of the highest profile cases often expose ideological beliefs that track with those philosophical or political categories. The Court meets in the United States Supreme Court Building in Washington, D.C.");
+    vga->puts("\r\n\n\n\n\n\n\n\n\nIn modern discourse");
+    vga->puts("\r\n\n\n\n\n\n\n\n\nmodern discourse");
+    vga->puts("\r\n\n\n\n\n\n\n\n\niscourse");
+    vga->puts("\r\n\n\n\n\n\n\n\n\nAmsterdam");
+    vga->puts("\r\n\n\n\n\n\n\n\n\nNederland");
+    vga->puts("The Court consists of a chief justice and eight associate ");
+    vga->puts("justices who are nominated by the President and confirmed by ");
+    vga->puts("the Senate. Once appointed, justices have life ");
+    vga->puts("tenure unless\r\nthey resign, retire, take senior status, or ");
+    vga->puts("are removed after impeachment (though none has ever been ");
+    vga->puts("\r\nNieuwe regel");
+    vga->puts("\r\nNieuwe regel");
+
 }
 
 int main()
