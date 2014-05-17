ef928affde8b7f01f0e1d1c11c8f6267926ca3fb
Jasper ter Weeme <jasper@nbalternate.lan>
2014-05-17 17:12:58 +0200

    bold red text

--- software/zuul1/kbtest1.cpp
+++ software/zuul1/kbtest1.cpp
@@ -1,6 +1,31 @@
+#include <system.h>
+#include "misc.h"
+
+class KeyboardTest
+{
+public:
+    void init();
+    int run();
+private:
+    Uart *uart;
+};
+
+void KeyboardTest::init()
+{
+    uart = Uart::getInstance();
+    uart->init((volatile uint32_t *)UART_BASE);
+}
+
+int KeyboardTest::run()
+{
+    uart->puts("\033[1;31mbold red text\033[0m\n");
+}
+
 int main()
 {
-    return 0;
+    KeyboardTest kbt;
+    kbt.init();
+    return kbt.run();
 }
 
 
