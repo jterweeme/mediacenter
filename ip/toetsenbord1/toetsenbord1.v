4bfd3896ae54184ae5777b33d58a383eb1c7a90a
Jasper ter Weeme <jasper@nbalternate.lan>
2014-05-18 02:00:22 +0200

    backup

--- ip/toetsenbord1/toetsenbord1.v
+++ ip/toetsenbord1/toetsenbord1.v
@@ -1,5 +1,31 @@
 module toetsenbord2(
-    clk
+    input clk,
+    input reset_n,
+    output reg irq,
+    input s_cs_n,
+    input s_read,
+    output [31:0] s_readdata,
+    input s_write,
+    input [31:0] s_writedata
 );
+
+always @(posedge clk or negedge reset_n) begin
+    if (~reset_n) begin
+        irq <= 1'b0;
+    end else begin
+        cnt <= cnt + 1;
+
+        if (cnt == 26'b1)
+            irq <= 1'b1;
+        else if (~s_cs_n & s_write)
+            irq <= 1'b0;
+
+    end
+end
+
+endmodule
+
+
+
 
 
--- software/zuul1/kbtest1.cpp
+++ software/zuul1/kbtest1.cpp
@@ -1,4 +1,5 @@
 #include <system.h>
+#include <priv/alt_legacy_irq.h>
 #include "misc.h"
 
 class KeyboardTest
@@ -6,19 +7,32 @@ class KeyboardTest
 public:
     void init();
     int run();
+    static void isrBridge(void *context);
 private:
     Uart *uart;
 };
+
+void KeyboardTest::isrBridge(void *context)
+{
+    Uart::getInstance()->puts("Interrupt\r\n");
+}
 
 void KeyboardTest::init()
 {
     uart = Uart::getInstance();
     uart->init((volatile uint32_t *)UART_BASE);
+    alt_irq_register(PS2_0_IRQ, 0, isrBridge);
+    //::alt_ic_isr_register(PS2_0_IRQ_INTERRUPT_CONTROLLER_ID, PS2_0_IRQ, isrBridge, 0, 0);
 }
 
 int KeyboardTest::run()
 {
-    uart->puts("\033[1;31mbold red text\033[0m\n");
+    uart->puts("\033[1;31mbold red text\033[0m\r\n");
+
+    while (true)
+    {
+        
+    }
 }
 
 int main()
