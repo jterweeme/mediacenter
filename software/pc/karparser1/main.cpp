c117dfb3608114fa11365e33f094e1a15cf537be
Jasper ter Weeme <jasper@nbweeme.lan>
2014-06-01 18:16:03 +0200

    betoh

--- software/pc/karparser1/main.cpp
+++ software/pc/karparser1/main.cpp
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
+
+#ifdef __GNUC__
 #include <endian.h>
+#endif
 
 struct Header
 {
@@ -18,8 +21,8 @@ struct Header
 class Utility
 {
 public:
-	static inline uint16_t byteswap16(uint16_t x);
-	static inline uint32_t byteswap32(uint32_t x);
+    static inline uint32_t be_32_toh(uint32_t x);
+    static inline uint16_t be_16_toh(uint16_t x);
 };
 
 class CHeader
@@ -62,18 +65,22 @@ void CTrack::read(FILE *file)
 {
     ::fread(&chunkID, sizeof(chunkID), 1, file);
     ::fread(&chunkSize, sizeof(chunkSize), 1, file);
-    ::fread(data, ::Utility::byteswap32(chunkSize), 1, file);
+    ::fread(data, ::Utility::be_32_toh(chunkSize), 1, file);
 }
 
-// wanneer niks gedefinieerd: little endian
-uint16_t Utility::byteswap16(uint16_t x)
+uint16_t Utility::be_16_toh(uint16_t x)
 {
+#ifdef __GNUC__
+    return be16toh(x);
+#endif
 	return (x & 0x00ff) << 8 | (x & 0xff00) >> 8;
 }
 
-uint32_t Utility::byteswap32(uint32_t x)
+uint32_t Utility::be_32_toh(uint32_t x)
 {
+#ifdef __GNUC__
     return be32toh(x);
+#endif
 	x = (x & 0x0000FFFF) << 16 | (x & 0xFFFF0000) >> 16;
 	return (x & 0x00FF00FF) << 8 | (x & 0xFF00FF00) >> 8;
 }
@@ -87,10 +94,10 @@ std::string CHeader::toString()
         ss << header.chunkID[i];
 
     ss << std::endl;
-	ss << "Chunk Size: " << ::Utility::byteswap32(header.chunkSize) << std::endl;
-	ss << "Format Type: " << ::Utility::byteswap16(header.formatType) << std::endl;
-	ss << "Tracks: " << ::Utility::byteswap16(header.tracks) << std::endl;
-	ss << "Time Division: " << ::Utility::byteswap16(header.timeDivision);
+	ss << "Chunk Size: " << ::Utility::be_32_toh(header.chunkSize) << std::endl;
+	ss << "Format Type: " << ::Utility::be_16_toh(header.formatType) << std::endl;
+	ss << "Tracks: " << ::Utility::be_16_toh(header.tracks) << std::endl;
+	ss << "Time Division: " << ::Utility::be_16_toh(header.timeDivision);
     return ss.str();
 }
 
