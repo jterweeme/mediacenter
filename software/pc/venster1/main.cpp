f17f33806a420ec2365478bedf8c234a298e6202
unknown <jterweeme@gmail.com>
2014-06-03 11:59:48 +0200

    backup

--- software/pc/venster1/main.cpp
+++ software/pc/venster1/main.cpp
@@ -1,5 +1,10 @@
-int WINAPI WinMain()
+#include <Windows.h>
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
+	return 0;
 }
 
 
