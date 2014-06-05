49e7a9e54b91e84e5068eaa76ef5135b62a2574c
Jasper ter Weeme <jasper@nbalternate.lan>
2014-06-05 16:44:22 +0200

    backup

--- ip/myduoseg1/myduoseg1.v
+++ ip/myduoseg1/myduoseg1.v
@@ -17,13 +17,8 @@ module slave_template(
 	output wire user_write,
 	output wire user_read
 );
-
-	parameter DATA_WIDTH = 16;
-	parameter ENABLE_SYNC_SIGNALS = 0;
-	parameter MODE_0 = 2;
-
-	wire [(DATA_WIDTH/8)-1:0] internal_byteenable;
-	reg [(DATA_WIDTH/8)-1:0] internal_byteenable_d1;
+	wire [15:0] internal_byteenable;
+	reg [1:0] internal_byteenable_d1;
 	wire [15:0] address_decode;
 	reg [15:0] address_decode_d1; 
 	wire [3:0] address_bank_decode;   
@@ -31,38 +26,30 @@ module slave_template(
     reg slave_read_d1;          
     reg slave_read_d2;           
     reg slave_write_d1;      
-	reg [DATA_WIDTH-1:0] mux_first_stage_a;
-	reg [DATA_WIDTH-1:0] mux_first_stage_b;
-	reg [DATA_WIDTH-1:0] mux_first_stage_c;
-	reg [DATA_WIDTH-1:0] mux_first_stage_d;
-	
-	generate
-		if (DATA_WIDTH == 8) begin
-			assign internal_byteenable = 1'b1;
-		end else begin
-			assign internal_byteenable = slave_byteenable;
-		end
-	endgenerate
-
-	assign address_decode[0] = (slave_address == 4'b0000) & (slave_write | slave_read);
-	assign address_decode[1] = (slave_address == 4'b0001) & (slave_write | slave_read);
-	assign address_decode[2] = (slave_address == 4'b0010) & (slave_write | slave_read);
-	assign address_decode[3] = (slave_address == 4'b0011) & (slave_write | slave_read);
-	assign address_decode[4] = (slave_address == 4'b0100) & (slave_write | slave_read);
-	assign address_decode[5] = (slave_address == 4'b0101) & (slave_write | slave_read);
-	assign address_decode[6] = (slave_address == 4'b0110) & (slave_write | slave_read);
-	assign address_decode[7] = (slave_address == 4'b0111) & (slave_write | slave_read);
-	assign address_decode[8] = (slave_address == 4'b1000) & (slave_write | slave_read);
-	assign address_decode[9] = (slave_address == 4'b1001) & (slave_write | slave_read);
-	assign address_decode[10] = (slave_address == 4'b1010) & (slave_write | slave_read);
-	assign address_decode[11] = (slave_address == 4'b1011) & (slave_write | slave_read);
-	assign address_decode[12] = (slave_address == 4'b1100) & (slave_write | slave_read);
-	assign address_decode[13] = (slave_address == 4'b1101) & (slave_write | slave_read);
-	assign address_decode[14] = (slave_address == 4'b1110) & (slave_write | slave_read);
-	assign address_decode[15] = (slave_address == 4'b1111) & (slave_write | slave_read);
-	assign address_bank_decode[0] = (address_decode_d1[3:0] != 0)? 1'b1 : 1'b0;
-	assign address_bank_decode[1] = (address_decode_d1[7:4] != 0)? 1'b1 : 1'b0;
-	assign address_bank_decode[2] = (address_decode_d1[11:8] != 0)? 1'b1 : 1'b0;
+	reg [15:0] mux_first_stage_a;
+	reg [15:0] mux_first_stage_b;
+	reg [15:0] mux_first_stage_c;
+    reg [15:0] mux_first_stage_d;
+    assign internal_byteenable = slave_byteenable;
+    assign address_decode[0] = (slave_address == 4'b0000) & (slave_write | slave_read);
+    assign address_decode[1] = (slave_address == 4'b0001) & (slave_write | slave_read);
+    assign address_decode[2] = (slave_address == 4'b0010) & (slave_write | slave_read);
+    assign address_decode[3] = (slave_address == 4'b0011) & (slave_write | slave_read);
+    assign address_decode[4] = (slave_address == 4'b0100) & (slave_write | slave_read);
+    assign address_decode[5] = (slave_address == 4'b0101) & (slave_write | slave_read);
+    assign address_decode[6] = (slave_address == 4'b0110) & (slave_write | slave_read);
+    assign address_decode[7] = (slave_address == 4'b0111) & (slave_write | slave_read);
+    assign address_decode[8] = (slave_address == 4'b1000) & (slave_write | slave_read);
+    assign address_decode[9] = (slave_address == 4'b1001) & (slave_write | slave_read);
+    assign address_decode[10] = (slave_address == 4'b1010) & (slave_write | slave_read);
+    assign address_decode[11] = (slave_address == 4'b1011) & (slave_write | slave_read);
+    assign address_decode[12] = (slave_address == 4'b1100) & (slave_write | slave_read);
+    assign address_decode[13] = (slave_address == 4'b1101) & (slave_write | slave_read);
+    assign address_decode[14] = (slave_address == 4'b1110) & (slave_write | slave_read);
+    assign address_decode[15] = (slave_address == 4'b1111) & (slave_write | slave_read);
+    assign address_bank_decode[0] = (address_decode_d1[3:0] != 0)? 1'b1 : 1'b0;
+    assign address_bank_decode[1] = (address_decode_d1[7:4] != 0)? 1'b1 : 1'b0;
+    assign address_bank_decode[2] = (address_decode_d1[11:8] != 0)? 1'b1 : 1'b0;
 	assign address_bank_decode[3] = (address_decode_d1[15:12] != 0)? 1'b1 : 1'b0;
 	
 	always @ (posedge clk or posedge reset) begin
@@ -99,7 +86,6 @@ module register_with_bytelanes(
 	input [1:0] byte_enables,
 	output reg [13:0] data_out
 );
-
     always @(posedge clk or posedge reset) begin
         if (reset == 1)
             data_out[6:0] <= 7'b0;
---
+++ ip/va1/va1.v
@@ -0,0 +1,6 @@
+module va1(
+    input csi_clk,
+    input csi_reset_n,
+    
+    output wire avs_s1_
+
