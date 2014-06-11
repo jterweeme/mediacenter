2de697967e3ce14f7c5d319230fceb3f4da79e41
Jasper ter Weeme <jasper@nbweeme.lan>
2014-06-11 13:32:03 +0200

    complex1 stub

--- software/pc/complex1/main.cpp
+++ software/pc/complex1/main.cpp
@@ -1,16 +1,49 @@
 #include <vector>
 #include <complex>
+#include <iostream>
+#include <sstream>
+
+class ComplexVector : public std::vector<std::complex<double> >
+{
+public:
+    std::string toString();
+    std::complex<double> sum();
+};
 
 class Complex1
 {
 public:
-    std::vector<std::complex<double> > onzin;
-    int run();
+    ComplexVector onzin;
+    int run(int argc, char **argv);
 };
+
+std::complex<double> ComplexVector::sum()
+{
+    std::complex<double> value;
+
+    for (iterator it = begin(); it != end(); ++it)
+        value += *it;
+
+    return value;
+}
+
+std::string ComplexVector::toString()
+{
+    std::ostringstream ss;
+    
+    for (iterator it = begin(); it != end(); ++it)
+        ss << *it << std::endl;
+
+    return ss.str();
+}
 
 int Complex1::run(int argc, char **argv)
 {
-    
+    onzin.push_back(std::complex<double>(4,4));
+    onzin.push_back(std::complex<double>(5,5));
+    std::cout << onzin.toString() << std::endl;
+    std::cout << onzin.sum() << std::endl;
+
     return 0;
 }
 
